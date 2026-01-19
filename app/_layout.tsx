2c011ecc9ead34b4d0a1b1f0dba405d71c02d3e7
Itzel <noviramos20@gmail.com>
2026-01-18 18:29:38 -0600

    V1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -13,7 +10,6 @@ export default function RootLayout() {
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
